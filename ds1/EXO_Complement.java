2298af52c4b4dbecfa0447a1a1517afab3c26f1c
You Nous <younss@mailoo.org>
2017-10-18 23:55:35 +0200

    1ere version exo sur ticket de caisse

--- ds1/EXO_Complement.java
+++ ds1/EXO_Complement.java
@@ -1,8 +1,27 @@
 class EXO_Complement extends Program{
+    void algorithm(){
+	ticketCaisse();
+    }
+    
     void ticketCaisse(){
-	
+	double prixArticle = 0.0, prixTotale = 0.0;
+	String nomProduit = "", mesArticles = "";
+	int quantiteArticle = 0;
+	do{
+	    print("Saisir le nom d'un article ou rien pour quitter: ");
+	    nomProduit = readString();
+	    if(equals(nomProduit,"")) break; 
+	    print("Saisir la quantité pour cet article            : ");
+	    quantiteArticle = readInt();
+	    print("Saisir le prix pour un article de [" + nomProduit + "]:  ");
+	    prixArticle = readDouble(); // problème avec readDouble , au lieu de . 
+	    mesArticles += ("(" + nomProduit + ", " + quantiteArticle + ", " + prixArticle + ")\n");
+	    prixTotale += (quantiteArticle*prixArticle);
+	}while( quantiteArticle != 0 && !equals(nomProduit,""));
+	println(mesArticles+
+		"-------------------------------------------------------\n"+
+		"\t\t\tvotre totale est de: " + prixTotale);
     }
-    void intersectionEnsemble(){
-	
-    }
+    // void intersectionEnsemble(){
+    // }
 }
