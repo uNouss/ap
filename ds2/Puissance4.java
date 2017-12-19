fd5572f83f2ae81125844a7f71b0d7a9a3ab2a0f
You Nous <younss@mailoo.org>
2017-12-19 13:08:55 +0100

    update mardi 19 décembre 2017, 13:08:55 (UTC+0100)

--- ds2/Puissance4.java
+++ ds2/Puissance4.java
@@ -48,8 +48,56 @@ class Puissance4 extends Program{
         return true;
     }
 
-    boolean estVictoire(){
+    int calculIdentiqueVerticale(char symbole, int row, int col){
+        int compteur = 1; // prise en compte de la case courante
+        int sens = 1;
+        boolean suffisant = false;
+        while( (row+sens) < length(grille, 1) && symbole == grille[row+sens][col] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens += 1;
+        }
+        if(suffisant) return compteur;
+        sens = -1;
+        while((row+sens) >= 0 && symbole == grille[row+sens][col] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens -= 1;
+        }
+        return compteur;
+    }
+
+    int calculIdentiqueHorizontale(char symbole, int row, int col){
+        int compteur = 1; // prise en compte de la case courante
+        int sens = 1;
+        boolean suffisant = false;
+        while( (col+sens) < length(grille, 2) && symbole == grille[row][col+sens] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens += 1;
+        }
+        if(suffisant) return compteur;
+        sens = -1;
+        while((col+sens) >= 0 && symbole == grille[row][col+sens] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens -= 1;
+        }
+        return compteur;
+    }
+
+    boolean estVictoire(int position){
+        int col, row = position/length(grille, 2), position%length(grille, 2);
+        char courant = grille[row][col];
+        // calculer verticalement le nombre d'occurence successive de la valeur courante
+        if( ( calculIdentiqueVerticale(courant, row, col) >= 4)
+            || ( calculIdentiqueHorizontale(courant, row, col) >= 4 )
+            || ( calculIdentiqueDiagonaleLTR(courant, row, col) >= 4 )
+            || ( calculIdentiqueDiagonaleRTL(courant, row, col) >= 4 ) ) return true;
+        else return false;
+        // calculer horizontalement le nombre d'occurence successive de la valeur courante
+        // calculer diagonalement dans le sens haut gauche bas droite le nombre d'occurence de la valeur courante
+        // calculer diagonalement dans le sens haut droit bas gauche le nombre d'occurence de la valeur courante
+        // et si on trouve un nombre d'au moins 4 occurances identiques dans un des sens, il y a victoire
         // essayer de simplifier le code en essayant de factoriser de partie de code semblable
+    }
+    boolean estVictoire_(){
         char courant, suivant = V, suivantDuSuivant = V, suivantDuSuivantDuSuivant = V;
         for(int idxL = 0; idxL < length(grille,1); idxL++){
             for(int idxC = 0; idxC < length(grille,2); idxC++){
@@ -124,33 +172,35 @@ class Puissance4 extends Program{
         return (joueur == 'X') ? 'O': 'X';
     }
 
-    void jouerALaColonne(char joueur, int col){
+    int jouerALaColonne(char joueur, int col){
         int pos = length(grille, 1) - 1;
         while(grille[pos][col] != V){
             pos -= 1;
         }
         grille[pos][col] = joueur;
+        return pos*length(grille, 2) + col;
     }
 
-    void joue(char joueur){
+    int joue(char joueur){
         int col = -1;
         do{
             print("JOUEUR_"+joueur+" "+PROMPT);
             col = readInt();
         }while(!estValide(col-1));
-        jouerALaColonne(joueur,col-1);
+        return jouerALaColonne(joueur,col-1);
     }
 
     void algorithm(){
         initialisationGrille();
         char joueur = ((int)(random()*2) == 1) ? 'O' : 'X'; // TIRAGE AU SORT DU DEBUTANT
-        while(!estPleine() && !estVictoire()){ // on joue
+        int position = -1;
+        do{ // on joue
             afficherGrille(); //afficher grille
-            joue(joueur); // le joueur courant joue
+            position = joue(joueur); // le joueur courant joue
             joueur = changer(joueur); // on change de joueur
-        }
+        }while(!estPleine() && !estVictoire(position));
         afficherGrille();
-        if(estVictoire()){
+        if(estVictoire(position)){
             println("JOUEUR_" + changer(joueur) + " a gagné");
         }else{
             println("match null");
