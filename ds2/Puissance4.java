6a33520d0055b787eedb9d9957776808a0984674
You Nous <younss@mailoo.org>
2017-12-19 16:39:19 +0100

    update mardi 19 décembre 2017, 16:39:19 (UTC+0100)

--- ds2/Puissance4.java
+++ ds2/Puissance4.java
@@ -82,79 +82,55 @@ class Puissance4 extends Program{
         return compteur;
     }
 
+    int calculIdentiqueDiagonaleLTR(char symbole, int row, int col){
+        int compteur = 1; // prise en compte de la case courante
+        int sens = 1;
+        boolean suffisant = false;
+        while( (row+sens) < length(grille, 1) && (col+sens) < length(grille, 2) && symbole == grille[row+sens][col+sens] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens += 1;
+        }
+        if(suffisant) return compteur;
+        sens = -1;
+        while( (row+sens) >= 0 && (col+sens) >= 0 && symbole == grille[row+sens][col+sens] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens -= 1;
+        }
+        return compteur;
+    }
+
+    int calculIdentiqueDiagonaleRTL(char symbole, int row, int col){
+        int compteur = 1; // prise en compte de la case courante
+        int sens = 1;
+        boolean suffisant = false;
+        while( (row-sens) >= 0 && (col+sens) < length(grille, 2) && symbole == grille[row-sens][col+sens] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens += 1;
+        }
+        if(suffisant) return compteur;
+        sens = -1;
+        while((row-sens) < length(grille, 1) && (col+sens) >= 0 && symbole == grille[row-sens][col+sens] && !(suffisant=(compteur >= 4))){
+            compteur += 1;
+            sens -= 1;
+        }
+        return compteur;
+    }
+
     boolean estVictoire(int position){
-        int col, row = position/length(grille, 2), position%length(grille, 2);
-        char courant = grille[row][col];
         // calculer verticalement le nombre d'occurence successive de la valeur courante
-        if( ( calculIdentiqueVerticale(courant, row, col) >= 4)
-            || ( calculIdentiqueHorizontale(courant, row, col) >= 4 )
-            || ( calculIdentiqueDiagonaleLTR(courant, row, col) >= 4 )
-            || ( calculIdentiqueDiagonaleRTL(courant, row, col) >= 4 ) ) return true;
-        else return false;
         // calculer horizontalement le nombre d'occurence successive de la valeur courante
         // calculer diagonalement dans le sens haut gauche bas droite le nombre d'occurence de la valeur courante
         // calculer diagonalement dans le sens haut droit bas gauche le nombre d'occurence de la valeur courante
         // et si on trouve un nombre d'au moins 4 occurances identiques dans un des sens, il y a victoire
         // essayer de simplifier le code en essayant de factoriser de partie de code semblable
-    }
-    boolean estVictoire_(){
-        char courant, suivant = V, suivantDuSuivant = V, suivantDuSuivantDuSuivant = V;
-        for(int idxL = 0; idxL < length(grille,1); idxL++){
-            for(int idxC = 0; idxC < length(grille,2); idxC++){
-                courant = grille[idxL][idxC];
-                if(courant != V){
-                    if(idxL+1 < length(grille, 1)){ // je fixe une colonne et varie la ligne à la recherche d'un alignement
-                        suivant = grille[idxL+1][idxC];
-                        if(idxL+2 < length(grille, 1)){
-                            suivantDuSuivant = grille[idxL+2][idxC];
-                            if( idxL+3 < length(grille, 1)){
-                                suivantDuSuivantDuSuivant = grille[idxL+3][idxC];
-                                if( courant == suivant
-                                        && suivant == suivantDuSuivant
-                                        && suivantDuSuivant == suivantDuSuivantDuSuivant ) return true;
-                            }
-                        }
-                    }
-                    if(idxC+1 < length(grille, 2)){ // je fixe une ligne et fait varier la colonne à la recherche d'un alignement
-                        suivant = grille[idxL][idxC+1];
-                        if(idxC+2 < length(grille, 2)){
-                            suivantDuSuivant = grille[idxL][idxC+2];
-                            if( idxC+3 < length(grille, 2)){
-                                suivantDuSuivantDuSuivant = grille[idxL][idxC+3];
-                                if( courant == suivant
-                                        && suivant == suivantDuSuivant
-                                        && suivantDuSuivant == suivantDuSuivantDuSuivant ) return true;
-                            }
-                        }
-                    }
-                    if(idxL+1 < length(grille,1) && idxC+1 < length(grille, 2)){ // je fais bouger ligne et colonne pour regarder dans la 1ere diagonale
-                        suivant = grille[idxL+1][idxC+1];
-                        if(idxL+2 < length(grille, 1) && idxC+2 < length(grille, 2)){
-                            suivantDuSuivant = grille[idxL+2][idxC+2];
-                            if(idxL+3 < length(grille, 1) && idxC+3 < length(grille, 2)){
-                                suivantDuSuivantDuSuivant = grille[idxL+3][idxC+3];
-                                if( courant == suivant
-                                        && suivant == suivantDuSuivant
-                                        && suivantDuSuivant == suivantDuSuivantDuSuivant) return true;
-                            }
-                        }
-                    }
-                    if(idxL+1 < length(grille,1) && idxC-1 >= 0){ // je fais bouger ligne et colonne pour regarder dans la 2nd diagonale
-                        suivant = grille[idxL+1][idxC-1];
-                        if(idxL+2 < length(grille,1) && idxC-2 >= 0){
-                            suivantDuSuivant = grille[idxL+2][idxC-2];
-                            if(idxL+3 < length(grille,1) && idxC - 3 >= 0){
-                                suivantDuSuivantDuSuivant = grille[idxL+3][idxC-3];
-                            if( courant == suivant
-                                    && suivant == suivantDuSuivant
-                                    && suivantDuSuivant == suivantDuSuivantDuSuivant) return true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+        int row = position/length(grille, 2);
+        int col = position%length(grille, 2);
+        char courant = grille[row][col];
+        if( ( calculIdentiqueVerticale(courant, row, col) >= 4)
+            || ( calculIdentiqueHorizontale(courant, row, col) >= 4 )
+            || ( calculIdentiqueDiagonaleLTR(courant, row, col) >= 4 )
+            || ( calculIdentiqueDiagonaleRTL(courant, row, col) >= 4 ) ) return true;
+        else return false;
     }
 
     boolean estPleineColonne(int col){
